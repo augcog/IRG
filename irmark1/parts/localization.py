f629975669d34d18c86cce8766b65a43d05b85e1
ritika <ritishubhi@gmail.com>
2020-03-03 12:58:20 -0800

    determinant doesn't work

--- irmark1/parts/localization.py
+++ irmark1/parts/localization.py
@@ -141,25 +141,30 @@ class Localization(object):
         self.cur_gray, self.cur_depth = gray, depth_arr
         
         positions_list = self.get_position_list(self.cur_gray)
+
+        # ############################
+        # #test keyframe matching
+        # if self.prev_gray is not None and self.cur_gray is not None:
+        #     R, T = self.keyframe_matching()
+        #     print("rotation matrix: ", R)
+        #     print("translation matrix: ", T)
+        #     if R is not None:
+        #         print("determinant: ", np.linalg.det(R))
+        #     # return self.prev_x, self.prev_y, self.prev_theta, False
+        # ###################################
+
         if (positions_list!=None):
             avg_x, avg_y, avg_z, avg_roll, avg_pitch, avg_yaw = self.avg_positions(positions_list)
             self.prev_x, self.prev_y, self.prev_z, self.prev_roll, self.prev_pitch, self.prev_yaw = avg_x, avg_y, avg_z, avg_roll, avg_pitch, avg_yaw
 
             print("runtime:", time.time()-start_time)
             return avg_x, avg_y, avg_z, avg_roll, avg_pitch, avg_yaw, True #Spherical Interp
-        elif self.prev_x == None:
-            return 0, 0, 0, 0,0,0, False
-        else: #KEYFRAME MATCHING
-            return  self.prev_x, self.prev_y, self.prev_z, self.prev_roll, self.prev_pitch, self.prev_yaw, False
-        # else:
-        #     R, T = self.keyframe_matching()
-        #     angle = np.arctan2(R[1, 0], R[0, 0])
-        #     pos = R * np.array([[self.prev_x], [self.prev_y], [1]]) + T
-
-        #     self.prev_x = pos[0]
-        #     self.prev_y = pos[1]
-        #     self.prev_theta += angle
-        #     return self.prev_x, self.prev_y, self.prev_theta, False
+        # elif self.prev_x == None:
+        #     return 0, 0, 0, 0,0,0, False
+        # else: #KEYFRAME MATCHING
+        #     return  self.prev_x, self.prev_y, self.prev_z, self.prev_roll, self.prev_pitch, self.prev_yaw, False
+      
+            
 
     def avg_positions(self, positions):
         norm = 0
@@ -176,173 +181,167 @@ class Localization(object):
             norm += 1/dist
         return avg_x/norm, avg_y/norm, avg_z/norm, avg_roll/norm, avg_pitch/norm, avg_yaw/norm
 
-    # def keyframe_matching(self):
-    #     """
-    #     Using keyframe matching to find the transformation between self.prev_gray and self.cur_gray.
-    #     :return: the transformation info, rotation and translation
-    #     """
-    #     # using BRIEF to find the feature descriptors
-    #     prev_kp, prev_des, cur_kp, cur_des = self.feature_extraction()
+    def keyframe_matching(self):
+        """
+        Using keyframe matching to find the transformation between self.prev_gray and self.cur_gray.
+        :return: the transformation info, rotation and translation
+        """
+        # using BRIEF to find the feature descriptors
+        prev_kp, prev_des, cur_kp, cur_des = self.feature_extraction()
 
-    #     # feature matching using brute-force and hamming distance
-    #     # prev_gray is the train image and cur_gray is the query image
-    #     matches = self.BFmatching(cur_des, prev_des)
+        # feature matching using brute-force and hamming distance
+        # prev_gray is the train image and cur_gray is the query image
+        matches = self.BFmatching(cur_des, prev_des)
 
-    #     # compute local 3D points
-    #     cur_p3d, prev_p3d = self.compute3D(matches, cur_kp, prev_kp)
+        # compute local 3D points
+        cur_p3d, prev_p3d = self.compute3D(matches, cur_kp, prev_kp)
 
-    #     # estimate the rigid transformation
-    #     R, t = self.estimate_rigid_transformation(cur_p3d, prev_p3d)
+        # estimate the rigid transformation
+        R, t = self.estimate_rigid_transformation(cur_p3d, prev_p3d)
 
-    #     return R, t
+        return R, t
 
-    # def feature_extraction(self):
-    #     """
-    #     Extract the features of self.prev_gray and self.cur_gray using CenSurE detector and BRIEF
-    #     :return: key points and descriptors of self.prev_gray and self.cur_gray
-    #     """
-    #     # Initiate STAR(CenSurE) detector and BRIEF extractor
-    #     star = cv2.xfeatures2d.StarDetector_create()
-    #     brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+    def feature_extraction(self):
+        """
+        Extract the features of self.prev_gray and self.cur_gray using CenSurE detector and BRIEF
+        :return: key points and descriptors of self.prev_gray and self.cur_gray
+        """
+        # Initiate STAR(CenSurE) detector and BRIEF extractor
+        star = cv2.xfeatures2d.StarDetector_create()
+        brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
 
-    #     # find the keypoints with STAR and descriptors of self.prev_gray
-    #     kp1 = star.detect(self.prev_gray, None)
-    #     kp1, des1 = brief.compute(self.prev_gray, kp1)
+        # find the keypoints with STAR and descriptors of self.prev_gray
+        kp1 = star.detect(self.prev_gray, None)
+        kp1, des1 = brief.compute(self.prev_gray, kp1)
 
-    #     # find the keypoints with STAR and descriptors of self.cur_gray
-    #     kp2 = star.detect(self.cur_gray, None)
-    #     kp2, des2 = brief.compute(self.cur_gray, kp2)
+        # find the keypoints with STAR and descriptors of self.cur_gray
+        kp2 = star.detect(self.cur_gray, None)
+        kp2, des2 = brief.compute(self.cur_gray, kp2)
 
-    #     return kp1, des1, kp2, des2
+        return kp1, des1, kp2, des2
 
-    # def BFmatching(self, des1, des2):
-    #     """
-    #     Matching des1 and des2 using brute-force matching and hamming distance
-    #     :param des1: query descriptors of self.cur_gray
-    #     :param des2: train descriptors of self.prev_gray
-    #     :return: sorted matches between des1 and des2
-    #     """
-    #     # print("type of des1:", type(des1))
-    #     # print(type(des2))
-    #     # create BFMatcher object
-    #     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    #     matches = bf.match(des1, des2)
+    def BFmatching(self, des1, des2):
+        """
+        Matching des1 and des2 using brute-force matching and hamming distance
+        :param des1: query descriptors of self.cur_gray
+        :param des2: train descriptors of self.prev_gray
+        :return: sorted matches between des1 and des2
+        """
+        # print("type of des1:", type(des1))
+        # print(type(des2))
+        # create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(des1, des2)
 
-    #     # Sort them in the order of their distance.
-    #     return sorted(matches, key=lambda x: x.distance)
+        # Sort them in the order of their distance.
+        return sorted(matches, key=lambda x: x.distance)
 
-    # def compute3D(self, matches, cur_kp, prev_kp):
-    #     """
-    #     compute the local 3d points
-    #     :param matches: the feature matched using BFmatching
-    #     :param prev_kp: the keypoints in self.prev_gray
-    #     :param cur_kp: the keypoints in self.cur_gray
-    #     :return: ndarray of points in 3D
-    #     """
-    #     cur_3d = []
-    #     prev_3d = []
-    #     for match in matches:
-    #         # x is the column number and y is the row number
-    #         (x1, y1) = cur_kp[match.queryIdx].pt
-    #         (x2, y2) = prev_kp[match.trainIdx].pt
+    def compute3D(self, matches, cur_kp, prev_kp):
+        """
+        compute the local 3d points
+        :param matches: the feature matched using BFmatching
+        :param prev_kp: the keypoints in self.prev_gray
+        :param cur_kp: the keypoints in self.cur_gray
+        :return: ndarray of points in 3D
+        """
+        cur_3d = []
+        prev_3d = []
+        for match in matches:
+            # x is the column number and y is the row number
+            (x1, y1) = cur_kp[match.queryIdx].pt
+            (x2, y2) = prev_kp[match.trainIdx].pt
 
-    #         cur_3d.append(self.get3D(x1, y1, self.cur_depth))
-    #         prev_3d.append(self.get3D(x2, y2, self.prev_depth))
+            cur_3d.append(self.get3D(x1, y1, self.cur_depth))
+            prev_3d.append(self.get3D(x2, y2, self.prev_depth))
 
-    #     return np.asarray(cur_3d), np.asarray(prev_3d)
+        return np.asarray(cur_3d), np.asarray(prev_3d)
 
-    # def get3D(self, x, y, depth):
-    #     """
-    #     compute the 3D point corresponding to (x, y)
-    #     :param x: x coordinate in 2d
-    #     :param y: y coordinate in 2d
-    #     :param depth: depth map for the whole image
-    #     :return: 3d coordinate
-    #     """
-    #     # print("x: ", x)
-    #     # print("y: ", y)
-    #     d = depth[int(y), int(x)]
-    #     x3d = (x - self.camera_matrix[0, 2]) * d / self.camera_matrix[0, 0]
-    #     y3d = (y - self.camera_matrix[1, 2]) * d / self.camera_matrix[1, 1]
-    #     return np.array([x3d, y3d, d])
+    def get3D(self, x, y, depth):
+        """
+        compute the 3D point corresponding to (x, y)
+        :param x: x coordinate in 2d
+        :param y: y coordinate in 2d
+        :param depth: depth map for the whole image
+        :return: 3d coordinate
+        """
+        d = depth[int(y), int(x)]
+        x3d = (x - self.camera_matrix[0, 2]) * d / self.camera_matrix[0, 0]
+        y3d = (y - self.camera_matrix[1, 2]) * d / self.camera_matrix[1, 1]
+        return np.array([x3d, y3d, d])
 
-    # def get_rigid_transformation3d(self, A, B):
-    #     """
-    #     Find rigid body transformation between A and B
-    #     :param A: 3 X n matrix of points
-    #     :param B: 3 x n matrix of points
-    #     :return: rigid body transformation between A and B
-    #     code is from https://github.com/nghiaho12/rigid_transform_3D/blob/master/rigid_transform_3D.py
-    #     solve the problem RA + t = B
-    #     """
-    #     assert len(A) == len(B)
-    #     num_rows, num_cols = A.shape;
-    #     if num_rows != 3:
-    #         raise Exception("matrix A is not 3xN, it is {}x{}".format(num_rows, num_cols))
-    #     [num_rows, num_cols] = B.shape;
-    #     if num_rows != 3:
-    #         raise Exception("matrix B is not 3xN, it is {}x{}".format(num_rows, num_cols))
+    def get_rigid_transformation3d(self, A, B):
+        """
+        Find rigid body transformation between A and B
+        :param A: 3 X n matrix of points
+        :param B: 3 x n matrix of points
+        :return: rigid body transformation between A and B
+        code is from https://github.com/nghiaho12/rigid_transform_3D/blob/master/rigid_transform_3D.py
+        solve the problem RA + t = B
+        """
+        assert len(A) == len(B)
+        num_rows, num_cols = A.shape;
+        if num_rows != 3:
+            raise Exception("matrix A is not 3xN, it is {}x{}".format(num_rows, num_cols))
+        [num_rows, num_cols] = B.shape;
+        if num_rows != 3:
+            raise Exception("matrix B is not 3xN, it is {}x{}".format(num_rows, num_cols))
 
-    #     # find mean column wise
-    #     centroid_A = np.mean(A, axis=1).reshape((3, 1))
-    #     centroid_B = np.mean(B, axis=1).reshape((3, 1))
+        # find mean column wise
+        centroid_A = np.mean(A, axis=1).reshape((3, 1))
+        centroid_B = np.mean(B, axis=1).reshape((3, 1))
 
-    #     # subtract mean
-    #     # Am = A - np.tile(centroid_A, (1, num_cols))
-    #     # Bm = B - np.tile(centroid_B, (1, num_cols))
-    #     Am = A - centroid_A
-    #     Bm = B - centroid_B
-    #     H = Am * np.transpose(Bm)
+        # subtract mean
+        # Am = A - np.tile(centroid_A, (1, num_cols))
+        # Bm = B - np.tile(centroid_B, (1, num_cols))
+        Am = A - centroid_A
+        Bm = B - centroid_B
+        H = Am @ np.transpose(Bm)
 
-    #     # find rotation
-    #     U, S, Vt = np.linalg.svd(H)
-    #     R = Vt.T * U.T
+        # find rotation
+        U, S, Vt = np.linalg.svd(H)
+        R = Vt.T * U.T
 
-    #     # special reflection case
-    #     if np.linalg.det(R) < 0:
-    #         print("det(R) < R, reflection detected!, correcting for it ...\n");
-    #         Vt[2, :] *= -1
-    #         R = Vt.T * U.T
+        # special reflection case
+        if np.linalg.det(R) < 0:
+            # print("det(R) < R, reflection detected!, correcting for it ...\n");
+            Vt[2, :] *= -1
+            R = Vt.T * U.T
 
-    #     # t = -R * centroid_A + centroid_B
-    #     t = centroid_B - np.matmul(R, centroid_A)
+        # t = -R * centroid_A + centroid_B
+        t = centroid_B - np.matmul(R, centroid_A)
 
-    #     return R, t
+        return R, t
 
-    # def estimate_rigid_transformation(self, cur_p3d, prev_p3d, num = 10, delta = 0.1):
-    #     """
-    #     find the best R, T between cur_p3d and prev_p3d
-    #     :param cur_p3d: n X 3 matrix of 3d points in the current gray image
-    #     :param prev_p3d: n X 3 matrix of 3d points in the previous gray image
-    #     :param num: max number of iterations
-    #     :param delta: bound used to check the quality of transformation
-    #     :return: best rigid transformation R, T
-    #     """
-    #     rotation = None
-    #     translation = None
-    #     accuracy = 0
+    def estimate_rigid_transformation(self, cur_p3d, prev_p3d, num = 100, delta = 15):
+        """
+        find the best R, T between cur_p3d and prev_p3d
+        :param cur_p3d: n X 3 matrix of 3d points in the current gray image
+        :param prev_p3d: n X 3 matrix of 3d points in the previous gray image
+        :param num: max number of iterations
+        :param delta: bound used to check the quality of transformation
+        :return: best rigid transformation R, T
+        """
+        rotation = None
+        translation = None
+        accuracy = 0
+        assert len(cur_p3d) == len(prev_p3d)
 
-    #     for i in range(num):
-    #         prev = np.random.permutation(prev_p3d).T # make it 3xn matrix
-    #         cur = np.random.permutation(cur_p3d).T
-    #         A = prev[:, :3]
-    #         B = cur[:, :3]
-    #         A1 = prev[:, 3:]
-    #         B1 = cur[:, 3:]
-
-    #         R, t = self.get_rigid_transformation3d(A, B)
-    #         # print("shape of R: ", R.shape)
-    #         # print("shape of t: ", t.shape)
-    #         # print("shape of A1, B1: ", A1.shape, B1.shape)
-    #         # print("shape of RA1: ", (np.matmul(R, A1) - t).shape)
-    #         error = np.linalg.norm(np.matmul(R, A1) + t - B1, axis = 1)
-    #         acc = np.sum(error < delta)
-    #         if acc > accuracy:
-    #             accuracy = acc
-    #             rotation = R
-    #             translation = t
+        for i in range(num):
+            # create 3 random indices of the correspondence points
+            idx = np.random.randint(len(cur_p3d), size=3)
+            A = prev_p3d[idx].T 
+            B = cur_p3d[idx].T
+           
+            R, t = self.get_rigid_transformation3d(A, B)
+        
+            error = np.linalg.norm(np.matmul(R, prev_p3d.T) + t - cur_p3d.T, axis = 0)
+            acc = np.sum(error < delta)
+            if acc > accuracy:
+                accuracy = acc
+                rotation = R
+                translation = t
             
-    #     return rotation, translation
+        return rotation, translation
 
 
     def shutdown(self):
